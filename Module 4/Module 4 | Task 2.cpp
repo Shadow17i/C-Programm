ec47ddd65ae9567cff6c056346c85c5fe4ea073c
Shadow <vtroyanok@gmail.com>
2026-01-21 15:34:20 +0300

    Update Module 4 | Task 2.cpp

--- Module 4/Module 4 | Task 2.cpp
+++ Module 4/Module 4 | Task 2.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    int sum;
+
+    cout << "Введите первое число ";
+    cin >> a;
+
+    cout << "Введите второе число ";
+    cin >> b;
+
+    cout << "Введите сумму ";
+    cin >> sum;
+
+    cout << "----------Проверяем--------" << endl;
+
+    if (a+b==sum) {
+        cout <<"Верно!";
+    } else {
+        cout << "Ошибка! Верный результат: " << a+b ;
+    }
+}
