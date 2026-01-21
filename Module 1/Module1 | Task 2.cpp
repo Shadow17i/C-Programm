51ee26f8d6cf5ae8ddc3a0b472f8c75aed2e23f3
Shadow17i <vtroyanok@gmail.com>
2026-01-21 13:37:38 +0300

    Update Module1 | Task 2.cpp

--- Module 1/Module1 | Task 2.cpp
+++ Module 1/Module1 | Task 2.cpp
@@ -1,1 +1,22 @@
+#include <iostream> 
 
+using namespace std;
+
+int main() { 
+    int price_product;
+    int price_delivery;
+    int discount;
+    int result;
+
+    cout << "Введите стоимость товара ";
+    cin >> price_product;
+
+    cout << "Введите стоимость доставки ";
+    cin >> price_delivery;
+
+    cout << "Введите скидку ";
+    cin >> discount;
+
+    result = price_product + price_delivery - discount;
+    cout << "Итоговая стоимость составляет " <<result;
+}
