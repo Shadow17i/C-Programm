80a1145fd977d44d7b9fef5d30bef06a5987d02a
Shadow <vtroyanok@gmail.com>
2026-01-21 16:57:15 +0300

    Update Module 4 | Task 6.cpp

--- Module 4/Module 4 | Task 6.cpp
+++ Module 4/Module 4 | Task 6.cpp
@@ -1,36 +1,51 @@
-#include <iostream>
+    #include <iostream>
 
-using namespace std;
+    using namespace std;
 
-int main() {
-    int count_man;
-    int count_barber;
-    int need_barber;
+    int main() {
+        int count_man;
+        int count_barber;
+        int need_barber;
 
 
-    cout << "Введите кол-во мужчин в городе: ";
-    cin >> count_man;
+        cout << "Введите кол-во мужчин в городе: ";
+        cin >> count_man;
 
-    cout << "Введите кол-во нанятых барберов: ";
-    cin >> count_barber;
-    
-    if (count_barber>count_man){ //проверка входных данных
-        cout << "Error";
-        return 0;
-    }
-    
-    if (count_barber!=0) // проверка если исходное кол-во барберов 0, т.к. делить на 0 нельзя
-    {
-        if (count_man%(count_barber*8)==0) { //если кол-во барберов совпадает вровент с кол-вом людей
-            cout <<"Нужно нанять ещё " <<count_man/(count_barber*8)-count_barber;
-        } else {  //если не совпадает
-            cout <<"Нужно нанять ещё " <<count_man/(count_barber*8)+1-count_barber;
+        cout << "Введите кол-во нанятых барберов: ";
+        cin >> count_barber;
+        
+
+
+        if (count_barber>count_man){ //проверка входных данных
+            cout << "Error";
+            return 0;
         }
-    } else {
-        if (count_man%8==0) {  //если кол-во барберов совпадает вровент с кол-вом людей
-            cout <<"Нужно нанять ещё " <<count_man/8;
-        } else { //если не совпадает
-            cout <<"Нужно нанять ещё " <<count_man/8+1;
+        
+
+
+        if (count_barber!=0) // проверка если исходное кол-во барберов 0, т.к. делить на 0 нельзя
+        {
+
+
+            if (count_man%(count_barber*8)==0) 
+            {
+                cout <<"Нужно нанять ещё " <<count_man/(count_barber*8)-count_barber; //если кол-во барберов совпадает вровент с кол-вом людей
+            } 
+            
+            else //если не совпадает
+            {  
+                cout <<"Нужно нанять ещё " <<count_man/(count_barber*8)+1-count_barber;
+            }
+        } 
+        else 
+        {
+            if (count_man%8==0) //если кол-во барберов совпадает вровент с кол-вом людей
+            {  
+                cout <<"Нужно нанять ещё " <<count_man/8;
+            } 
+            else //если не совпадает
+            { 
+                cout <<"Нужно нанять ещё " <<count_man/8+1;
+            }
         }
     }
-}
