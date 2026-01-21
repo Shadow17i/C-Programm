4a564903cecd715106c6b066245c486a451b7908
Shadow <vtroyanok@gmail.com>
2026-01-21 15:16:14 +0300

    Update Module 4 | Task 1.cpp

--- Module 4/Module 4 | Task 1.cpp
+++ Module 4/Module 4 | Task 1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+    
+    cout << "Введите первое число ";
+    cin >> a;
+
+    cout << "Введите второе число ";
+    cin >> a;
+
+    cout << "----------Проверяем--------" << endl;
+
+    if (a>b) {
+        cout <<"Наименьшее число: "<< b;
+    } else if (b>a){
+        cout << "Наименьшее число: " << a;
+    } else {
+        cout << "Числа равны" ;
+    }
+}
