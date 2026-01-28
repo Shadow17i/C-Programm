c8b2bdf80ae8a268558f8d8f9e8b3b61b79cbd55
Shadow <vtroyanok@gmail.com>
2026-01-28 17:01:52 +0300

    Update Module 7 | Task 1.cpp

--- Module 7/Module 7 | Task 1.cpp
+++ Module 7/Module 7 | Task 1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int month;
+    int buckwheat_weight=100;
+
+    cout << "Всего гречки было на начало подсчёта:"<< endl;
+    for (month = 1; month < buckwheat_weight/4; month ++) {
+        cout << "После " << month << " месяца у вас в запасе останется " << buckwheat_weight-month*4<<endl;
+    }
+    cout << "После "<< month <<" месяца гречка закончится"<<endl;
+
+}
